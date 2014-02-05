79a9c02ada0f39caef1296c7c05fc32fa090df8f
Javier Otegui <javier.otegui@gmail.com>
2014-02-05 17:07:37 -0500

    Finishing Day 2

--- maxent_data.R
+++ maxent_data.R
@@ -5,11 +5,11 @@
 #One pound for code
 
 ###Load libraries
-#library(dismo)
-#library(rgdal)
-#library(maptools)
-#library(sp)
-#library(raster)
+library(dismo)
+library(rgdal)
+library(maptools)
+library(sp)
+library(raster)
 
 #############################################################################
 ### download all worldclim data
@@ -17,13 +17,52 @@
 
 ### Below is all of the data for 2.5 degree minute data (~4.5 km). If we want 30 sec (~1km), then we'll have to specify tiles with the species extent.
 
-setwd("C:/Users/Nicole/Desktop/wc2-5")
-bioclimP <- brick("wc2-5.grd")
-plot(bioclimP)
+data_folder = '/home/jotegui/Desktop/NESCentBHLEOL/EOLBHL/data/'
+setwd(data_folder)
+
+### Present
+setwd(paste(data_folder,"bioclim/wc2-5/wc2-5", sep="/"))
+
+bioclim_P1<-("./bio1.bil")
+raster(bioclim_P1)
+
+bioclim_P2<-("./bio2.bil")
+raster(bioclim_P2)
+
+bioclim_P3<-("./bio3.bil")
+raster(bioclim_P3)
+
+bioclim_P4<-("./bio4.bil")
+raster(bioclim_P4)
+
+bioclim_P5<-("./bio5.bil")
+raster(bioclim_P5)
+
+bioclim_P6<-("./bio6.bil")
+raster(bioclim_P6)
+
+bioclim_P7<-("./bio7.bil")
+raster(bioclim_P7)
+
+bioclim_P12<-("./bio12.bil")
+raster(bioclim_P12)
+
+bioclim_P13<-("./bio13.bil")
+raster(bioclim_P13)
+
+bioclim_P14<-("./bio14.bil")
+raster(bioclim_P14)
+
+bioclim_P15<-("./bio15.bil")
+raster(bioclim_P15)
+
+bioclim_P<-stack(bioclim_P1,bioclim_P2,bioclim_P3,bioclim_P4,bioclim_P5,bioclim_P6,bioclim_P7,bioclim_P12,bioclim_P13,bioclim_P14, bioclim_P15)
+plot(bioclim_P)
 
 ###Future
-setwd("C:/Users/Nicole/Desktop/future_bioclim")
-bioclimF <- stack("future_bioclim")
+setwd(paste(data_folder,"bioclim/future_bioclim", sep="/"))
+
+#bioclimF <- stack("future_bioclim")
 
 bioclim1<-("./cc26bi501.tif")
 raster(bioclim1)
@@ -83,16 +122,17 @@ bioclim19<-("./cc26bi5019.tif")
 raster(bioclim19)
 
 bioclimF<-stack(bioclim1,bioclim2,bioclim3,bioclim4,bioclim5,bioclim6,bioclim7,bioclim8,bioclim9,bioclim10,bioclim11,bioclim12,bioclim13,bioclim14, bioclim15, bioclim16, bioclim17, bioclim18,bioclim19)
-bioclimF<-brick(bioclim1,bioclim2,bioclim3,bioclim4,bioclim5,bioclim6,bioclim7,bioclim8,bioclim9,bioclim10,bioclim11,bioclim12,bioclim13,bioclim14, bioclim15, bioclim16, bioclim17, bioclim18,bioclim19)
+#bioclimF<-brick(bioclim1,bioclim2,bioclim3,bioclim4,bioclim5,bioclim6,bioclim7,bioclim8,bioclim9,bioclim10,bioclim11,bioclim12,bioclim13,bioclim14, bioclim15, bioclim16, bioclim17, bioclim18,bioclim19)
 
-bioclimF<-brick(bioclimF)
+#bioclimF<-brick(bioclimF)
 bioclimF
-writeRaster(bioclimF, filename="bioclimFuture")
+#writeRaster(bioclimF, filename="bioclimFuture")
 
 plot(bioclimF)
 
 ###Elevation (same in present and future)
-setwd("C:/Users/Nicole/Desktop/elevation")
+#setwd("C:/Users/Nicole/Desktop/elevation")
+setwd(paste(data_folder,"bioclim/elevation", sep="/"))
 ele <- brick("./alt.bil")
 plot(ele)
 
@@ -144,7 +184,6 @@ plot(ele)
 eRunP<-evaluate(pres_test, runP, bioclimP)
 ###Map?
 pred_map <- predict(runP, bioclim)
-
 
 
 
